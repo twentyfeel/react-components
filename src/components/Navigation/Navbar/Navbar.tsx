56b430c859aab587cd37110f23ce31d5d4af5ac2
Slobodan Zivanovic <192605563+zslobodan@users.noreply.github.com>
2025-01-01 15:01:32 +0100

    Base (init) navbar

---
+++ src/components/Navigation/Navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+import { NavbarProps } from "./props/NavbarProps.ts";
+
+const Navbar: React.FC<NavbarProps> = () => {
+  return (
+    <div>
+      <h1>Navbar</h1>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/components/Navigation/Navbar/props/NavbarProps.ts
@@ -0,0 +1,3 @@
+export type NavbarProps = {
+  logo?: string;
+};
